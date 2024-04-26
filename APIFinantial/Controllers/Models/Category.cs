5d09e28e0a655aeecf77e35a744f15b6755a5171
wallasce <lwallasce@outlook.com>
2024-04-26 11:31:34 -0300

    Feat: Create Model Category

---
+++ APIFinantial/Controllers/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace APIFinantial.Controllers.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string? Name { get; set; }
+
+        public enum Type
+        {
+            Deposits,
+            Debits
+        }
+
+        public Type CategoryType { get; set; }
+    }
+}
