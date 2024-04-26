e4d06d5ac628cfeb16886d823ded482034df267d
wallasce <lwallasce@outlook.com>
2024-04-26 14:57:57 -0300

    Feat: Create Account and User

---
+++ APIFinantial/Controllers/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace APIFinantial.Controllers.Models;
+
+public class Account
+{
+    public int AccountId { get; set; }
+    public required string Bank { get; set; }
+    public double Balance { get; set; }
+
+    public List<MoneyMovement>? MoneyMovements { get; set; }
+
+    public UserData? OwnerAccount { get; set; }
+}
---
+++ APIFinantial/Controllers/Models/UserData.cs
@@ -0,0 +1,7 @@
+﻿namespace APIFinantial.Controllers.Models;
+
+public class UserData 
+{
+    public int UserDataId { get; set; }
+    public List<Account>? Accounts { get; set; }
+}
