e6506a34f01109c6a445327361b0b6e3d856402a
wallasce <lwallasce@outlook.com>
2024-04-26 13:49:40 -0300

    Feat: Create Model Money Moviment

---
+++ APIFinantial/Controllers/Models/MoneyMovement.cs
@@ -0,0 +1,10 @@
+﻿namespace APIFinantial.Controllers.Models
+{
+    public abstract class MoneyMovement
+    {
+        public required double Value { get; set; }
+        public required DateOnly Date { get; set; }
+        public required string Description { get; set; }
+        public required Category Category { get; set; }
+    }
+}
