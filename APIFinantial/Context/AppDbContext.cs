90d0fcd21bf377bf2ee3d31fa71c9a9221588b28
wallasce <lwallasce@outlook.com>
2024-04-29 09:35:42 -0300

    Feat: Create Context and configure database

---
+++ APIFinantial/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using APIFinantial.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace APIFinantial.Context;
+
+public class AppDbContext : DbContext
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<Income> Incomes { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<UserData> UserDatas { get; set; }
+}
