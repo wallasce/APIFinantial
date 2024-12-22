2e7851f01366c265214cbc842e5f4a4bbae93475
wallasce <lwallasce@outlook.com>
2024-12-22 15:37:46 -0300

    🎉 Begin project

---
+++ src/main/java/com/api/finantial/FinantialApplication.java
@@ -0,0 +1,13 @@
+package com.api.finantial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinantialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinantialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/finantial/FinantialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.finantial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinantialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
